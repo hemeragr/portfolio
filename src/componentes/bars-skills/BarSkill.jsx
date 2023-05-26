bd40d64ab90e6842dea90d7adad544d39ce40ad4
hemeragr <hemeraguerrero@hotmail.com>
2023-05-26 13:42:41 +0200

    componente menu creado y añadido, componentes de barras de skills,

---
+++ src/componentes/bars-skills/BarSkill.jsx
@@ -0,0 +1,14 @@
+import IconHtml from "../icons/bar-skills/icons-skills/iconHtml";
+import "./BarSkills.scss";
+
+const BarSkill = () => {
+  return (
+    <>
+      <div className="bar-skill">
+        <IconHtml /> <BarSkill />
+      </div>
+    </>
+  );
+};
+
+export default BarSkill;
