44d2766f3d7885fad02fdd9a72c5ca8f3e58a205
hemeragr <hemeraguerrero@hotmail.com>
2023-05-26 15:58:41 +0200

    solucion error

--- src/componentes/bars-skills/BarSkill.jsx
+++ src/componentes/bars-skills/BarSkill.jsx
@@ -1,11 +1,22 @@
-import IconHtml from "../icons/bar-skills/icons-skills/iconHtml";
+import Bar50 from "../icons/bar-skills/Bar50";
+import IconHtml from "../icons/bar-skills/icons-skills/IconHtml";
 import "./BarSkills.scss";
+import Bar25 from "../icons/bar-skills/Bar75"
+import Bar75 from "../icons/bar-skills/Bar25"
+import Bar100 from "../icons/bar-skills/Bar100"
+
 
 const BarSkill = () => {
   return (
     <>
       <div className="bar-skill">
-        <IconHtml /> <BarSkill />
+        <IconHtml />
+        <Bar25 />
+        <Bar50 />
+        <Bar75 />
+        
+        <Bar100 />
+      
       </div>
     </>
   );
