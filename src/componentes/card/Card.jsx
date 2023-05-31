41c00a0c8e82291ddc1e37360b4d2128b1be27cc
hemeragr <hemeraguerrero@hotmail.com>
2023-05-31 20:05:12 +0200

    tarjets-work

---
+++ src/componentes/card/Card.jsx
@@ -0,0 +1,23 @@
+import "./Card.scss";
+
+const Card = ({ cita }) => {
+  return (
+    <>
+      <div className="card">
+        <div className="card-body">
+          <h3 className="card-title headers">{cita.work}</h3>
+          <p className="card-text paragraph">{cita.description}</p>
+          <p className="card-skills paragraph">{cita.skills_used}</p>
+          <button href="#" className="btn button-send">
+            {" "}
+            <a href="#" className="link">
+              &gt;
+            </a>
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Card;
--- src/views/work/Work.jsx
+++ src/views/work/Work.jsx
@@ -1,7 +1,41 @@
+import Card from "../../componentes/card/Card";
 import Nav from "../../componentes/nav/Nav";
 import "./Work.scss";
 
 const Work = () => {
+  const DB = [
+    {
+      work: "Trabajo 1",
+      description: "Trabajo about",
+      skills_used: "#css #html #js",
+    },
+    {
+      work: "Trabajo 2",
+      description: "Trabajo super chulo hecho con react",
+      skills_used: "#css #html",
+    },
+    {
+      work: "Trabajo 3",
+      description: "Trabajo figma",
+      skills_used: "#js #react",
+    },
+    {
+      work: "Trabajo 4",
+      description: "Trabajo html y css",
+      skills_used: "#vue #figma",
+    },
+    {
+      work: "Trabajo 5",
+      description: "Trabajo calculadora",
+      skills_used: "#css #sumas",
+    },
+    {
+      work: "Trabajo 6",
+      description: "Trabajo super chulo hecho wordpress",
+      skills_used: "#php #wprdpress",
+    },
+  ];
+
   return (
     <>
       <body>
@@ -9,7 +43,13 @@ const Work = () => {
           <header className="top-bar">
             <Nav />
           </header>
-          <main></main>
+          <main className="content">
+            {DB.map((obj) => (
+              <div className="content-card" key={obj.work}>
+                <Card cita={obj} />
+              </div>
+            ))}
+          </main>
           <footer></footer>
         </section>
         <section className="content-img"></section>
